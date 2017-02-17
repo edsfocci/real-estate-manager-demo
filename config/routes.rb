80bc7fd363414dd4dbb5c7b8092387f9743284a7
Ed Solis <edsfocci@gmail.com>
2017-02-17 01:57:21 -0600

    Constrain the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   devise_for :users, controllers: { sessions:       'sessions',
                                     registrations:  'registrations' }
-  # namespace :api, defaults: { format: :json } do
-  #   resources 'subscriptions'
-  #   resources 'properties'
-  # end
-  resources 'subscriptions', defaults: { format: :json }
-  resources 'properties', defaults: { format: :json }
+
+  resources 'subscriptions',  defaults: { format: :json }, only: :update
+  resources 'properties',     defaults: { format: :json },
+                                              only: [:create, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
